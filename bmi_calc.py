cf2911e794aa25f8d7bbe6c8a8290af834a1eaf1
Wendy Yansah <wendycoolzzz74@gmail.com>
2021-10-30 05:14:44 +0700

    Initial commit

---
+++ bmi_calc.py
@@ -0,0 +1,23 @@
+print("Hello! This is a BMI (Body Mass Index) Calculator.\nThis will helps you measure your body fat based on your height and weight!")
+print("Let's get started!\n")
+
+height = float(input("Enter your height (cm) : "))
+weight = float(input("Enter your weight (kg) : "))
+
+BMI = weight / (height / 100)**2
+BMI_shw = "{:.2f}".format(BMI)
+
+print(f"Your BMI is {BMI_shw}.\n")
+
+if BMI <= 18.4:
+    print("You are underweight.")
+elif BMI <= 24.9:
+    print("You are healthy.")
+elif BMI <= 29.9:
+    print("You are over weight.")
+elif BMI <= 34.9:
+    print("You are severely over weight.")
+elif BMI <= 39.9:
+    print("You are obese.")
+else:
+    print("You are severely obese.")
